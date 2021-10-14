7be926bc03c0ff432288d542f66a1034ecb18f3d
Dishika Tayal <dishikatayal@microsoft.com>
2021-10-14 14:51:08 +0530

    new changes

--- Program.cs
+++ Program.cs
@@ -12,13 +12,17 @@ namespace ConsoleApp1
 {
     class Program
     {
-        private const string connectionString = "";
+        //private const string connectionString = "Endpoint=sb://fdass-perf.servicebus.windows.net/;SharedAccessKeyName=ReadWrite;SharedAccessKey=T8SOtgUJOp2rO/G8mYR9VWh5LMA6Zb1u6htOl9CmPKk=";
 
-        private const string eventHubName = "";
+        //private const string eventHubName = "hackathon";
 
-        private const string blobStorageConnectionString = "";
+        //private const string blobStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=fdaasperfehcheckpoints;AccountKey=A8UsjTFC9VMsh2W84vQHwK2XsjLlR7GwrgNQ5+oHftUV5Q5Eo4cp9uxNQjL802F4XTnHTruxgRKIIkpMVR9c4w==;EndpointSuffix=core.windows.net";
 
-        private const string blobContainerName = "";
+        //private const string blobContainerName = "hackathon";
+        private const string connectionString = "Endpoint=http://localhost/;";// SharedAccessKeyName=DummyAccessKeyName;SharedAccessKey=5dOntTRytoC24opYThisAsit3is2B+OGY1US/fuL3ly="; 
+        private const string eventHubName = "hackathon";
+
+
 
         static async Task Main()
         {
@@ -26,14 +30,16 @@ namespace ConsoleApp1
             // Create a producer client that you can use to send events to an event hub
             await using (var producerClient = new EventHubProducerClient(connectionString, eventHubName))
             {
-                
+
                 //Thread.Sleep(10000);
                 //sleep ----------------------
                 // Create a batch of events 
+                Console.WriteLine("Hello1");
 
                 using EventDataBatch eventBatch = await producerClient.CreateBatchAsync();
-                
+
                 //sleep-------------------------
+                Console.WriteLine("Hello2");
 
                 // Add events to the batch. An event is a represented by a collection of bytes and metadata. 
                 eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("First event")));
@@ -42,7 +48,7 @@ namespace ConsoleApp1
 
                 // Use the producer client to send the batch of events to the event hub
                 Console.WriteLine("going to sleep");
-                Thread.Sleep(10000);
+                //Thread.Sleep(10000);
                 await producerClient.SendAsync(eventBatch);
                 Console.WriteLine("A batch of 3 events has been published.");
             }
